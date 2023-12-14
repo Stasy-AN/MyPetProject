e58b224ec8822f1152b761ec56e52b007eb700d3
Stasy-AN <nasta2909@mail.ru>
2023-12-14 16:09:19 +0900

    Обновление от 14.12

--- CatMy/main/urls.py
+++ CatMy/main/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index, name='home'),
+    path('news/', views.news, name='news'),
     path('account/',views.account, name='account'),
     path('contakt/',views.contakt, name='contakt'),
     path('cats/',views.cats, name='cats'),
